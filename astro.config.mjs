1f662dcb97c9e3131d0579940fe62d347fb7e9d2
Rex-8 <rehaanjosemathew@gmail.com>
2025-07-05 00:09:49 +0530

    feat: Initial Astro setup with MDX, Tailwind, and GitHub Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Rex-8.github.io', // Your full website URL
-  base: '/nh-web', // The base path for your repo (Rex-8.github.io/nh-web)
+  // Your full website URL
+  site: 'https://Rex-8.github.io',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'], 
+
+  // The base path for your repo (Rex-8.github.io/nh-web)
+  base: '/nh-web',
+
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
