b8a9c7e3cc123d897f76ae3ea114a20692d7003b
Rex-8 <rehaanjosemathew@gmail.com>
2025-07-05 00:02:01 +0530

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Rex-8.github.io', // Your full website URL
+  base: '/nh-web', // The base path for your repo (Rex-8.github.io/nh-web)
+});
